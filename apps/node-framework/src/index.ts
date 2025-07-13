1537759e414e34b5f3bdb9b9373d9aedbff43a3f
coder7475 <robiulhossain7475@gmail.com>
2025-07-13 23:46:50 +0600

    feat(node-framework): create node server

--- apps/node-framework/src/index.ts
+++ apps/node-framework/src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello Node framwork");
+import { createServer, IncomingMessage, ServerResponse } from "node:http";
+
+const requestListener = (req: IncomingMessage, res: ServerResponse) => {
+  const path = req.url;
+  const method = req.method;
+
+  console.log(`${method} ${path}`);
+
+  res.writeHead(200, {
+    "Content-Type": "text/plain",
+  });
+  res.write("Hello Node Server!");
+  res.end();
+};
+
+// listen
+const server = createServer(requestListener);
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
