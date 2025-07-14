1d1b0283e02323b6caa76288166485be035b54e8
coder7475 <robiulhossain7475@gmail.com>
2025-07-14 19:33:43 +0600

    feat(express): improve error handler

--- apps/express/src/middlewares/globalErrorHandler.ts
+++ apps/express/src/middlewares/globalErrorHandler.ts
@@ -1,15 +1,18 @@
+import { logger } from "@/app";
 import { env } from "@/configs/envConfig";
-import type { ErrorRequestHandler } from "express";
+import type { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 
-const globalErrorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
-  const statusCode: number = err.statusCode ?? 500;
-  const message: string = err.message ?? "Internal Server Error";
+const globalErrorHandler: ErrorRequestHandler = (err, _req: Request, res: Response, _next: NextFunction) => {
+	const statusCode: number = err.statusCode ?? 500;
+	const message: string = err.message ?? "Internal Server Error";
 
-  res.status(statusCode).json({
-    success: false,
-    message,
-    stack: env.NODE_ENV === "production" ? undefined : err.stack,
-  });
+	logger.error(err);
+
+	res.status(statusCode).json({
+		success: false,
+		message,
+		stack: env.NODE_ENV === "production" ? undefined : err.stack,
+	});
 };
 
 export default globalErrorHandler;
