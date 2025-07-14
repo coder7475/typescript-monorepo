7b1d584a63aec3e7e1e774d52179c90893e3f275
coder7475 <robiulhossain7475@gmail.com>
2025-07-14 22:16:02 +0600

    feat: udpate logger

--- apps/express/src/utils/asyncHandler.ts
+++ apps/express/src/utils/asyncHandler.ts
@@ -1,11 +1,11 @@
 import type { NextFunction, Request, Response } from "express";
+import { logger } from "@/app";
 
 type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>;
 
 export const catchAsync = (fn: AsyncHandler) => (req: Request, res: Response, next: NextFunction) => {
-  Promise.resolve(fn(req, res, next)).catch((err) => {
-    console.log(err);
-    next(err);
-  });
+	Promise.resolve(fn(req, res, next)).catch((err) => {
+		logger.error(err);
+		next(err);
+	});
 };
-
