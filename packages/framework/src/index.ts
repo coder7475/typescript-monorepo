c6096d13d5a788a91c8cd64d3f7ed89c07d71173
coder7475 <robiulhossain7475@gmail.com>
2025-07-15 19:30:49 +0600

    refactor(db): restructure database module by moving mongoConnector to a new file and removing unused add function

--- packages/framework/src/index.ts
+++ packages/framework/src/index.ts
@@ -1,1 +1,21 @@
+import { Server } from "./server.js";
+
+export class Framework {
+  server: Server;
+  constructor() {
+    this.server = new Server();
+  }
+
+  // methods
+  get() {}
+
+  post() {}
+
+  listen(port: number, cb?: () => void) {
+    this.server.listen(port, cb);
+  }
+}
+
+export const createApp = () => new Framework();
+
 export * from "./server.js";
