4d3ef0f6db3c3b5f377b506f97cf9e69fefc6494
coder7475 <robiulhossain7475@gmail.com>
2025-07-15 19:21:49 +0600

    feat(server): add event listeners for request handling and server startup logging

--- apps/node-framework/src/index.ts
+++ apps/node-framework/src/index.ts
@@ -1,3 +1,18 @@
 import { Server } from "@repo/framework/server";
 
 const server = new Server();
+
+// events
+server.on("request:received", () => {
+  console.log("Request Received!");
+});
+
+server.on("request:processed", () => {
+  console.log("Request Processed!");
+});
+
+// listener
+const port = 5555;
+server.listen(port, () => {
+  console.log(`Server is Running on port http://localhost:${port}!`);
+});
