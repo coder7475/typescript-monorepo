b64c19fe4f143ca94fc26e4ebd236955534324a9
coder7475 <robiulhossain7475@gmail.com>
2025-07-15 19:14:39 +0600

    feat(node-framework): integrate framework package and update TypeScript configuration

--- apps/node-framework/src/index.ts
+++ apps/node-framework/src/index.ts
@@ -1,29 +1,3 @@
-import { createServer, IncomingMessage, ServerResponse } from "node:http";
+import { Server } from "@repo/framework/server";
 
-const requestListener = (req: IncomingMessage, res: ServerResponse) => {
-  const path = req.url;
-  const method = req.method;
-
-  console.log(`${method} ${path}`);
-
-  const response: Record<string, any> = {};
-
-  if (path?.startsWith("api/v1")) {
-    response.statusCode = 200;
-  }
-
-  res.writeHead(200, {
-    "Content-Type": "Application/json",
-  });
-  res.write(JSON.stringify(response));
-  res.end();
-};
-
-// listen
-const server = createServer(requestListener);
-
-const port = 5000;
-
-server.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+const server = new Server();
