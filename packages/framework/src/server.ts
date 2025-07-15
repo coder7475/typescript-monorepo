42e27f51925c89706c1851e8f2d215d026a27356
coder7475 <robiulhossain7475@gmail.com>
2025-07-15 18:47:19 +0600

    feat(server): implement listen method and enhance request handling with structured response

--- packages/framework/src/server.ts
+++ packages/framework/src/server.ts
@@ -11,9 +11,31 @@ export class Server extends EventEmitter {
     this.server = createServer(this.handleRequest.bind(this));
   }
 
-  // listener
+  // methods
+  public listen(port: number, cb?: () => void) {
+    this.server.listen(port, cb);
+  }
+
   private handleRequest(nodeReq: IncomingMessage, nodeRes: ServerResponse) {
     this.emit("request:received");
+
+    // * Step 1: Process the request and response and create our own req, res object
+
+    // * Step 2: Parse the body
+
+    // * Step 3: Match the Route
+
+    // * Step 4: Execute the handler or Execute the middleware chain
+    const response = {
+      statusCode: 200,
+      message: "Hello World!",
+    };
+
+    nodeRes.writeHead(200, {
+      "Content-Type": "Application/json",
+    });
+    nodeRes.write(JSON.stringify(response));
+    nodeRes.end();
 
     this.emit("request:processed");
   }
