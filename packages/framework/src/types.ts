e3ec76fb0418467cdbb8b59e463b229287dd01b5
coder7475 <robiulhossain7475@gmail.com>
2025-07-15 12:24:12 +0600

    feat(types): enhance Request interface with additional properties for headers, path, query, params, and body

--- packages/framework/src/types.ts
+++ packages/framework/src/types.ts
@@ -5,7 +5,14 @@ import { IncomingMessage } from "node:http";
  */
 export type Method = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 
+// contract for request
 export interface Request {
   nodeReq: IncomingMessage;
   method: Method;
+  headers: Record<string, string>;
+  path: string; // actual path post/1234
+  originalPath: string; // template path post/:id
+  query: Record<string, string>;
+  params: Record<string, string>;
+  body: string | Object | null;
 }
