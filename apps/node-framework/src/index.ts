53089b181dd96dd87ea1ab8584c0c955424cb3fa
coder7475 <robiulhossain7475@gmail.com>
2025-07-16 11:37:17 +0600

    feat(server): add event listeners for request received and processed events

--- apps/node-framework/src/index.ts
+++ apps/node-framework/src/index.ts
@@ -1,7 +1,14 @@
 import { createApp } from "@repo/framework";
 
 const app = createApp();
+// events
+app.server.on("request:received", () => {
+  console.log("Server Request Received!");
+});
 
+app.server.on("request:processed", () => {
+  console.log("Server Request Processed!");
+});
 // listener
 const port = 5555;
 app.listen(port, () => {
