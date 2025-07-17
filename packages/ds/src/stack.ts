48e32c3cd821bf2264210b6e4cb8802738c1fdae
coder7475 <robiulhossain7475@gmail.com>
2025-07-17 11:34:24 +0600

    feat(ds): implement ArrayStack class with push, pop, peek, isEmpty, and isFull methods

--- packages/ds/src/stack.ts
+++ packages/ds/src/stack.ts
@@ -1,1 +1,39 @@
-export const add = (a: number, b: number) => a + b;
+// Base: Array
+export class ArrayStack {
+  stack;
+  top: number;
+  constructor(public maxSize: number = 10) {
+    this.stack = new Array(maxSize);
+    this.top = -1;
+  }
+
+  // methods
+  push(data: unknown) {
+    if (this.top >= this.maxSize - 1) {
+      throw new Error("Stack Overflow");
+    }
+    this.stack[++this.top] = data;
+  }
+
+  isEmpty() {
+    return this.top == -1;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      throw new Error("Stack Underflow");
+    }
+    return this.stack[this.top--];
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      throw new Error("Stack Underflow");
+    }
+    return this.stack[this.top];
+  }
+
+  isFull() {
+    return this.top === this.maxSize - 1;
+  }
+}
