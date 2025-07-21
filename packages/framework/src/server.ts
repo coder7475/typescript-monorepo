5f5e18b21bdc40ddcc37f246ec25a792f3749f98
coder7475 <robiulhossain7475@gmail.com>
2025-07-21 19:30:45 +0600

    feat(server): add HTTP method handlers (GET, POST, PUT, PATCH, DELETE) to Server class for improved routing capabilities

--- packages/framework/src/server.ts
+++ packages/framework/src/server.ts
@@ -4,6 +4,7 @@ import { createServer, IncomingMessage, ServerResponse } from "node:http";
 import { RequestImp } from "./Request.js";
 import { ResponseImp } from "./Response.js";
 import { Router } from "./Router.js";
+import { Handler } from "./types.js";
 
 // Event Driven Server
 export class Server extends EventEmitter {
@@ -21,12 +22,30 @@ export class Server extends EventEmitter {
     this.server.listen(port, cb);
   }
 
+  public get(path: string, handler: Handler) {
+    this.router.add("GET", path, handler);
+  }
+
+  public post(path: string, handler: Handler) {
+    this.router.add("POST", path, handler);
+  }
+
+  public put(path: string, handler: Handler) {
+    this.router.add("PUT", path, handler);
+  }
+
+  public patch(path: string, handler: Handler) {
+    this.router.add("PATCH", path, handler);
+  }
+
+  public delete(path: string, handler: Handler) {
+    this.router.add("DELETE", path, handler);
+  }
+
   private async handleRequest(
     nodeReq: IncomingMessage,
     nodeRes: ServerResponse<IncomingMessage>,
   ) {
-    this.emit("request:received");
-
     // * Step 1: Process the request and response and create our own req, res object
     const request = new RequestImp(nodeReq);
     const response = new ResponseImp(nodeRes);
